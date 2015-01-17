46f374f413d8fd36bec998d19c0934bd181a028f
FireiMedia <fireimedia@gmail.com>
2015-01-17 14:57:04 +0100

    new class Effekte

---
+++ src/li/angu/firei/luckyblocks/events/Effekte.java
@@ -0,0 +1,6 @@
+package li.angu.firei.luckyblocks.events;
+
+public enum Effekte {
+	Diamond, LuckyBlocks, Explosion, Redstoner, Enchanter, LavaBlock, SilverFish, GoldArmor, Stadtmusikanten, Loch, Tower, Jump, Zombie, Music, LeatherArmor, GoldSword;
+	
+}
