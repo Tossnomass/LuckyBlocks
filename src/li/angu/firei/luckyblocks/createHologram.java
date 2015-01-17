7ceddba48e7692be1edfee78b49cfd7455a8f90a
FireiMedia <fireimedia@gmail.com>
2015-01-17 15:11:35 +0100

    new animation

--- src/li/angu/firei/luckyblocks/createHologram.java
+++ src/li/angu/firei/luckyblocks/createHologram.java
@@ -10,15 +10,23 @@ public class createHologram {
 
 	Main plugin = Main.instance;
 
-	public createHologram(String msg, Location loc) {
+	public createHologram(String msg, final Location loc) {
 		final Hologram hologram = HologramsAPI.createHologram(plugin, loc);
 		hologram.appendTextLine(msg);
+		final int move = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+			
+			@Override
+			public void run() {
+				hologram.teleport(loc.add(0, 0.2, 0));
+				
+			}
+		}, 10, 10);
 		Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
 			
 			@Override
 			public void run() {
 				hologram.delete();
-				
+				Bukkit.getScheduler().cancelTask(move);
 			}
 		},20L*5);
 	}
