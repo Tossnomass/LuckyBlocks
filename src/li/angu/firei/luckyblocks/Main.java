c4034b113abf7598d94e4ecc8c3f256343b7dfe5
FireiMedia <fireimedia@gmail.com>
2015-01-17 14:43:46 +0100

    better overview

--- src/li/angu/firei/luckyblocks/Main.java
+++ src/li/angu/firei/luckyblocks/Main.java
@@ -1,5 +1,7 @@
 package li.angu.firei.luckyblocks;
 
+import li.angu.firei.listener.LISTENERBlockBreak;
+import li.angu.firei.luckyblocks.commands.CMDaction;
 import li.angu.firei.luckyblocks.events.EventManager;
 import li.angu.firei.luckyblocks.events.EventManager.Effects;
 import net.md_5.bungee.api.ChatColor;
@@ -15,61 +17,28 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.ItemSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener{
+public class Main extends JavaPlugin {
 
 	public static Main instance;
-	
-	
-	
+
 	@Override
 	public void onEnable() {
-		
+
 		if (!Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays")) {
-	        getLogger().severe("*** HolographicDisplays is not installed or not enabled. ***");
-	        getLogger().severe("*** This plugin will be disabled. ***");
-	        this.setEnabled(false);
-	        return;
-	    }
-		
+			getLogger()
+					.severe("*** HolographicDisplays is not installed or not enabled. ***");
+			getLogger().severe("*** This plugin will be disabled. ***");
+			this.setEnabled(false);
+			return;
+		}
+
 		instance = this;
-		
-		getServer().getPluginManager().registerEvents(this, this);
-		
+
+		new LISTENERBlockBreak(this);
+
+		CMDaction action = new CMDaction(this);
+		getCommand("action").setExecutor(action);
+
 	}
-	
-	@EventHandler
-	private void onSpawn(ItemSpawnEvent e){
-	}
-	
-	@EventHandler
-	private void onBreak(BlockBreakEvent e){
-		if(e.getBlock().getType() == Material.SEA_LANTERN){
-			e.setCancelled(true);
-			e.getBlock().setType(Material.AIR);
-			EventManager.playEffect(e.getBlock().getLocation());
-		}
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd,
-			String label, String[] args) {
-		
-		Player p = (Player) sender;
-		
-		
-		if(cmd.getName().equalsIgnoreCase("test")){
-			if(args.length == 1){
-				Effects e = Effects.valueOf(args[0]);
-				EventManager.playEffect(e, p.getLocation());
-				p.sendMessage(ChatColor.AQUA + "Effekt: " + e.name());
-				return true;
-			}
-			Effects e = EventManager.playEffect(p.getLocation());
-			p.sendMessage(ChatColor.AQUA + "Effekt: " + e.name());
-		}
-		
-		return true;
-	}
-	
-	
+
 }
